041257320222da87088b54034c28dbe5fd5b14fa
Raphael Menard <raphael.menard@ynov.com>
2024-02-05 16:58:12 +0100

    chore: fix rebase

--- griffonage.io/src/pages/Game.tsx
+++ griffonage.io/src/pages/Game.tsx
@@ -15,7 +15,7 @@ const Game = () => {
         <h1 className="text-center text-5xl mt-12 mb-8">P'tite game</h1>
             <GameCanvas />
             <Chat />
-        <Link to="/"><button type="button" className="m-10 p-2 border-4 border-black rounded-2xl">Retour</button></Link>
+        <Link to="/"><button type="button" className="m-10 p-2 border-4 border-black rounded-2xl" onClick={endGame}>Retour</button></Link>
         {
             (Word) ?
                 <WordToGuess/>
